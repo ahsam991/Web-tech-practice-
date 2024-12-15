167b119c95aa5ae46eeb968f0b4c743686a6bd6a
MD AHSAMUL HAQUE <139666656+ahsam991@users.noreply.github.com>
2024-12-15 14:45:31 +0600

    Create login.php

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <!-- Login Section -->
+    <h2>LOGIN</h2>
+    <form action="login.php" method="post">
+        <label for="user_id">User Id</label><br>
+        <input type="text" id="user_id" name="user_id"><br><br>
+
+        <label for="password">Password</label><br>
+        <input type="password" id="password" name="password"><br><br>
+
+        <button type="submit">Login</button>
+        <a href="register.html">Register</a>
+    </form>
+</body>
+</html>
